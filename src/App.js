d2d62069466e1e3f47218c34fa074a1caa8f274a
Bhaskar Vegesna <bakuraju@gmail.com>
2025-02-22 00:36:08 +1100

    Added button components with classnames and tailwind-merge

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import Button from "./Button";
+import { GoBell, GoCode, GoBrowser, GoBug, GoDatabase } from "react-icons/go";
+
+function App() {
+  return (
+    <div>
+      <h1>React Components</h1>
+      <div className="grid grid-cols-1 divide-y-4 divide-yellow-200">
+        <div>
+          <Button primary outline className="mb-5">
+            <GoBell />
+            Click Me!
+          </Button>{" "}
+        </div>
+        <div>
+          <Button secondary rounded>
+            <GoCode />
+            One
+          </Button>{" "}
+        </div>
+        <div>
+          <Button success outline rounded>
+            <GoBrowser />
+            Two
+          </Button>{" "}
+        </div>
+        <div>
+          <Button warning rounded>
+            <GoDatabase />
+            Three
+          </Button>{" "}
+        </div>
+        <div>
+          <Button danger>
+            <GoBug />
+            Four
+          </Button>{" "}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
